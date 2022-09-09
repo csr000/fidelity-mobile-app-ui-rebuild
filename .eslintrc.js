9664c049b7319ed81fd288e78eadd9030cebcf00
csr000 <csrichard000@gmail.com>
2022-09-10 00:14:49 +0100

    ✏️ eslint corrections

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   parser: '@babel/eslint-parser',
-  parserOptions : {
-    "ecmaVersion": 8,
-    "requireConfigFile": false,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "impliedStrict": true,
-      "classes": true
+  parserOptions: {
+    ecmaVersion: 8,
+    requireConfigFile: false,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      impliedStrict: true,
+      classes: true,
     },
   },
   extends: 'universe/native',
