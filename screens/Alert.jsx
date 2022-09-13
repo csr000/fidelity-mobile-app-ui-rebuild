8aaf96517c6a511c2bdaef4b95ee67207cadb5fe
csr000 <csrichard000@gmail.com>
2022-09-13 19:10:57 +0100

    add layout

--- screens/Alert.jsx
+++ screens/Alert.jsx
@@ -1,12 +1,61 @@
+import { FlatList, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import Block from '../components/Block';
 import Text from '../components/Text';
+import { colors } from '../components/theme';
 
-export default function Home() {
+const screenWidth = Dimensions.get('screen').width;
+
+function Item({ title }) {
   return (
-    <Block middle center flex={1}>
-      <Text bold>ALERT</Text>
+    <Block middle center margin={10} width={screenWidth - 20} height={100} borderRadius={20} backgroundColor={colors.gray}>
+      <Text>{title}</Text>
     </Block>
   );
 }
 
-// const styles = StyleSheet.create({})
+export default function Alert() {
+  const DATA = [
+    {
+      id: '1',
+      title: ' Item',
+    },
+    {
+      id: '2',
+      title: ' Item',
+    },
+    {
+      id: '3',
+      title: ' Item',
+    },
+    {
+      id: '4',
+      title: ' Item',
+    },
+    {
+      id: '5',
+      title: ' Item',
+    },
+    {
+      id: '6',
+      title: ' Item',
+    },
+    {
+      id: '7',
+      title: ' Item',
+    },
+    {
+      id: '8',
+      title: ' Item',
+    },
+  ];
+  const renderItem = ({ item }) => <Item title={item.title} />;
+  return (
+    <SafeAreaView>
+      <Block center>
+        <FlatList data={DATA} renderItem={renderItem} keyExtractor={(item) => item.id} />
+      </Block>
+    </SafeAreaView>
+  );
+}
