6079ff5cd2ab98bbec3f53bf4ad0f68c0488db7b
JasmineBabaYara <jassyprincess1@icloud.com>
2022-09-13 14:18:27 -0700

    tabbar icons included

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,9 +1,12 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { AntDesign, Entypo } from "expo-vector-icons";
 
 import Alert from '../screens/Alert';
 import Home from '../screens/Home';
 import Setting from '../screens/Setting';
 import Transaction from '../screens/Transaction';
+import { colors } from '../components/theme';
+import { flushSync } from 'react-dom';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +15,24 @@ export default function TabNavigation() {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: colors.white,
+        tabBarInactiveTintColor: colors.gray,
+        tabBarShowLabel:false,
+        tabBarStyle: {
+          backgroundColor: colors.black,
+          height:130,
+          borderRadius:40,
+        },
       }}>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          // tabBarIcon:({}) =>(
+          //   <Entypo name="home" size={30} color={color} />
+          // )
+        }}
+      />
       <Tab.Screen name="Transaction" component={Transaction} />
       <Tab.Screen name="Setting" component={Setting} />
       <Tab.Screen name="Alert" component={Alert} />
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,42 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Image } from 'react-native';
 import Block from '../components/Block';
 import Text from '../components/Text';
+import qrcode from '../assets/img/qrcode.png'
 
 export default function Home() {
   return (
-    <Block middle center flex={1}>
-      <Text bold>HOME</Text>
+    <SafeAreaView style={styles.homecontainer}>
+    <Block style={styles.header}>
+      {/* beginning of header section */}
+      <Block>
+        <Text>Fidelity</Text>
+        <Text>Bank</Text>
+      </Block>
     </Block>
+
+    <Block>
+      <Text>Hello, Richard</Text>
+    </Block>
+    
+    <Block>
+      <Text>Mr.</Text>
+      <Text>Antwi Richard</Text>
+      <Block>
+        <Text>My Accounts</Text>
+        <Block>
+        <Image source={qrcode} />
+        </Block>
+      </Block>
+    </Block>
+    </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  homecontainer: {
+    height:'100%',
+    width:'100%',
+    display:'flex',
+  }
+})
