41e4dd6e634e28b2779f33bafc890258de9d7402
JasmineBabaYara <jassyprincess1@icloud.com>
2022-09-14 18:02:36 -0700

    add tabbar icons

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -30,9 +30,9 @@ function Row({ title1, title2, title3 }) {
 function SubCard(props) {
   const { image, name, amount } = props;
   return (
-    <Block backgroundColor={colors.black} marginTop={20} height={130} width={120} padding={10} borderRadius={15}>
+    <Block backgroundColor={colors.black} marginTop={10} height={130} width={110} padding={10} borderRadius={15}>
       <Block>
-        <Image source={image} style={{ width: 100, height: 60 }} />
+        <Image source={image} style={{ width: 90, height: 60 }} />
       </Block>
       <Text color={colors.white} style={{ marginTop: 10 }}>
         {name}
@@ -44,7 +44,7 @@ function SubCard(props) {
 export default function Home() {
   return (
     <SafeAreaView style={{ height: '100%', width: '100%', display: 'flex' }}>
-      <Block row spacebetween margin={20}>
+      <Block row spacebetween marginLeft={15}>
         {/* icons will be inserted */}
         <Block row>
           <Text bold size={20}>
@@ -54,14 +54,14 @@ export default function Home() {
         </Block>
       </Block>
 
-      <Block middle center row marginTop={10}>
+      <Block middle center row marginTop={8}>
         <Text style={{ fontSize: 30, color: colors.gray }}>Hello, </Text>
         <Text bold style={{ fontSize: 30 }}>
           John
         </Text>
       </Block>
 
-      <Block center width="90%" height={160} margin={20} borderRadius={10} backgroundColor={colors.black}>
+      <Block center width="92%" height={160} margin={15} borderRadius={10} backgroundColor={colors.black}>
         <Text style={{ marginTop: 10, color: colors.gray, fontSize: 16 }}>Mr.</Text>
         <Text bold style={{ marginTop: 7, color: colors.white, fontSize: 25 }}>
           John Doe
@@ -78,16 +78,16 @@ export default function Home() {
 
       <Block backgroundColor={colors.gray} height="50%" width="100%" borderRadius={20} marginTop={0} style={{ position: 'absolute', bottom: '0%' }} />
 
-      <Block backgroundColor={colors.black} margin={20} borderRadius={10} width="90%">
+      <Block backgroundColor={colors.black} margin={15} borderRadius={10} width="92%">
         <Row title1="Transfers" title2="Payments" title3="Airtime & Data" />
         <Row title1="My Payees" title2="Account Services" title3="View All" />
       </Block>
 
-      <Block margin={20}>
+      <Block marginLeft={15}>
         <Text size={20} bold>
           Your Cards
         </Text>
-        <Block row spacebetween>
+        <Block row spacebetween marginRight={15}>
           <SubCard name="Visa Card" amount="$1000.99" image={visa} />
           <SubCard name="Master Card" amount="$100.07" image={mastercard} />
           <SubCard name="Master Card" amount="$100.07" />
