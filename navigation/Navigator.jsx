4c90b9ca8cb28b4cea7378059525c7f935e1addf
JasmineBabaYara <jassyprincess1@icloud.com>
2022-09-16 22:33:53 -0700

    animate tabbar

--- navigation/Navigator.jsx
+++ navigation/Navigator.jsx
@@ -1,36 +1,83 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SimpleLineIcons, Foundation, Feather } from 'expo-vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 import Alert from '../screens/Alert';
 import Home from '../screens/Home';
 import Setting from '../screens/Setting';
 import Transaction from '../screens/Transaction';
 import { colors } from '../components/theme';
+import Block from '../components/Block';
 
 const Tab = createBottomTabNavigator();
+
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+  var isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <Block flex={1} style={{ alignItems: 'center' }}>
+        <TouchableOpacity
+          style={{
+            bottom: 18,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: colors.white,
+            shadowColor: colors.black,
+            shadowOpacity: 8,
+            shadowOffset: {
+              height: 0,
+              width: 0,
+            },
+            shadowRadius: 13,
+            elevation: 5,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </Block>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+        }}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
+};
 
 export default function TabNavigation() {
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: colors.white,
+        tabBarActiveTintColor: colors.orange,
         tabBarInactiveTintColor: colors.gray,
         tabBarShowLabel: false,
         tabBarStyle: {
-          backgroundColor: colors.black,
-          height: 70,
+          backgroundColor: colors.white,
           borderTopRightRadius: 30,
           borderTopLeftRadius: 30,
-          Outline: 'none',
           position: 'absolute',
+          height: 70,
+          bottom: 0,
+          right: 0,
+          left: 0,
         },
       }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ color }) => <SimpleLineIcons name="home" size={30} color={color} />
+          tabBarIcon: ({ color }) => <SimpleLineIcons name="home" size={30} color={color} />,
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
@@ -38,15 +85,25 @@ export default function TabNavigation() {
         component={Transaction}
         options={{
           tabBarIcon: ({ color }) => <Foundation name="clipboard-notes" size={30} color={color} />,
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
         }}
       />
-      <Tab.Screen name="Setting" component={Setting} options={{
-          tabBarIcon: ({ color }) =><Feather name="settings" size={30} color={color} />,
-        }}  />
-      <Tab.Screen name="Alert" component={Alert} 
-      options={{
-        tabBarIcon: ({ color }) => <Feather name="message-square" size={30} color={color} />,
-      }} />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarIcon: ({ color }) => <Feather name="settings" size={30} color={color} />,
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Alert"
+        component={Alert}
+        options={{
+          tabBarIcon: ({ color }) => <Feather name="message-square" size={30} color={color} />,
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
