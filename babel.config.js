f7609af6a7b4564ac155f5610b343901cdd7cd32
csr000 <csrichard000@gmail.com>
2022-09-17 04:13:58 +0100

    📦 add reanimated and gesture-handler

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
