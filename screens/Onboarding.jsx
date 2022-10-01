f52b71df0e9f7020a78b4d122ee13cef041df8d9
JasmineBabaYara <jassyprincess1@icloud.com>
2022-09-30 23:21:21 -0700

    update ui

--- screens/Onboarding.jsx
+++ screens/Onboarding.jsx
@@ -1,111 +1,124 @@
-import { Image, TouchableOpacity } from 'react-native';
+import { Image, TouchableOpacity, SafeAreaView, FlatList, Dimensions, Animated, View } from 'react-native';
 
 import Onboarding from 'react-native-onboarding-swiper';
+import data from '../services/onboarding/data';
 import { colors } from '../components/theme';
 import Block from '../components/Block';
 import Text from '../components/Text';
+import { useRef, useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const Dots = ({ selected }) => {
-  let backgroundColor;
+const { width } = Dimensions.get('window');
 
-  backgroundColor = selected ? '#f58326' : 'transparent';
-  return (
-    <Block
-      style={{
-        width: 10,
-        height: 10,
-        marginHorizontal: 5,
-        backgroundColor,
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: colors.orange,
-      }}
-    />
-  );
-};
-
-const Skip = ({ ...props }) => (
-  <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
-    <Text size={12}>Skip for now</Text>
-  </TouchableOpacity>
-);
-
-const Continue = ({ ...props }) => (
-  <TouchableOpacity
-    style={{
-      padding: 15,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-    {...props}>
-    <Text color={colors.black} size={12}>
-      Continue
-    </Text>
-  </TouchableOpacity>
-);
-
-const Done = ({ ...props }) => (
-  <TouchableOpacity
-    style={{
-      marginHorizontal: 10,
-      height: 40,
-      width: 70,
-      padding: 5,
-      borderTopLeftRadius: 20,
-      borderBottomLeftRadius: 20,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-    {...props}>
-    <Text color={colors.black} size={12}>
-      Done
-    </Text>
-  </TouchableOpacity>
+//render item for flatlist
+const renderItem = ({ item }) => (
+  <Block center>
+    <Image source={item.image} style={{ resizeMode: 'contain', width, justifyContent: 'center', marginTop: 10 }} />
+    <Block>
+      <Text center size={28} bold color={colors.orange} style={{ marginBottom: 10 }}>
+        {item.title}
+      </Text>
+      <Text center size={18} color={colors.gray} style={{ paddingHorizontal: 64 }}>
+        {item.description}
+      </Text>
+    </Block>
+  </Block>
 );
 
 const OnboardingScreen = ({ navigation }) => {
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const Ref = useRef(null);
+
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+  
+
+  //scroll to next slide function
+  const scrollTo = () => {
+    if (currentIndex < data.length - 1) {
+      Ref.current.scrollToIndex({ index: currentIndex + 1 });
+    }
+  };
+
+  //pagination 
+  const Paginator = () => {
+    return (
+      <Block row height={100} middle>
+        {data.map((_, i) => {
+          const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
+          const dotWidth = scrollX.interpolate({
+            inputRange,
+            outputRange: [8, 40, 8],
+            extrapolate: 'clamp',
+          });
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.3, 1, 0.3],
+            extrapolate: 'clamp',
+          });
+          return (
+            <Animated.View
+              key={i.toString()}
+              style={[
+                { height: 4, borderRadius: 5, backgroundColor: colors.orange, marginHorizontal: 8 },
+                { width: dotWidth, opacity },
+              ]}
+            />
+          );
+        })}
+      </Block>
+    );
+  };
+
+
   return (
-    <Onboarding
-      SkipButtonComponent={Skip}
-      NextButtonComponent={Continue}
-      DoneButtonComponent={Done}
-      DotComponent={Dots}
-      bottomBarColor="rgba(251, 192, 147, 0.9)"
-      //bottomBarHighlight={false}
-      titleStyles={{
-        color: colors.black,
-      }}
-      subTitleStyles={{
-        color: colors.black,
-      }}
-      imageContainerStyles={
-        {
-          //paddingTop: 200,
+    <SafeAreaView style={{ backgroundColor: colors.black, height: '100%' }}>
+      <Block style={{ backgroundColor: colors.white, height: '90%', borderBottomLeftRadius: 30, borderBottomRightRadius: 30 }}>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          bounces={false}
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+            useNativeDriver: false,
+          })}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={Ref}
+        />
+        {Paginator()}
+      </Block>
+
+      <Block center>
+        {currentIndex == data.length -1 ? (
+        <TouchableOpacity style={{ width: '60%', height: '23%', backgroundColor: colors.orange, marginTop: -23, justifyContent: 'center', alignItems: 'center', borderRadius: 30 }} onPress={()=> scrollTo()}>
+          <Text color={colors.white} size={16}>
+            Done
+          </Text>
+        </TouchableOpacity>
+        ) :(
+        <TouchableOpacity style={{ width: '60%', height: '23%', backgroundColor: colors.orange, marginTop: -23, justifyContent: 'center', alignItems: 'center', borderRadius: 30 }} onPress={()=> scrollTo()}>
+          <Text color={colors.white} size={16}>
+            Next
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=> navigation.navigate('TabNavigation')}>
+          <Text color={colors.white} size={16} style={{ marginTop: 10 }}>
+            Skip for now
+          </Text>
+        </TouchableOpacity>)
         }
-      }
-      onSkip={() => navigation.replace('TabNavigation')}
-      onDone={() => navigation.navigate('TabNavigation')}
-      pages={[
-        {
-          backgroundColor: '#fff',
-          image: <Image source={require('../assets/img/credit_payment.png')} />,
-          title: 'Credit Card Payments',
-          subtitle: 'Demo',
-        },
-        {
-          backgroundColor: '#fff',
-          image: <Image source={require('../assets/img/mobile_money.png')} />,
-          title: 'Mobile Pay',
-          subtitle: 'Demo',
-        },
-        {
-          backgroundColor: '#fff',
-          image: <Image source={require('../assets/img/transfer_money.png')} />,
-          title: 'Online Transactions',
-          subtitle: 'Demo',
-        },
-      ]}
-    />
+        
+        
+      </Block>
+    </SafeAreaView>
   );
 };
 
